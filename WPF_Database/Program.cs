3c4ec0bc5e4770f2e79d2742f2e7a819c7e49218
Tóth Kevin <tothkevin0716@gmail.com>
2023-12-07 10:46:16 +0100

    Create Program.cs

---
+++ WPF_Database/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WPF_Database
+{
+    public class Program
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            EmployeeService employeeService = new EmployeeService();
+            if (args.Contains("--count"))
+            {
+                Console.WriteLine("A dolgozók száma: " + employeeService.GetAll().Count);
+            }
+            else
+            {
+                Application application = new Application();
+                application.Run(new MainWindow(employeeService));
+            }
+        }
+    }
+}
